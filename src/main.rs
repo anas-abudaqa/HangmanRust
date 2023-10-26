887d14c8121b2744fa07035dfe207d68c54eee93
Anas Abu Dagga <abudaqa.anas@gmail.com>
2023-10-27 00:56:01 +0200

    Still testing

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@ use std::io;
 fn main() {
     println!("Welcome to Hangman");
 
-    let mut guess = String::new();
-    let chosen_word = String::from("Impression");
+    //let mut guess = String::new();
     //let mut WORD_LENGTH: i32 = chosen_word.chars().count();
-    let tries = 6; 
+    let tries = 6;
+    let game_word = String::from("Penis");
 
-    
-    create_game_word();
-    println!("Guess a number!");
+    let mut hangman = Game{
+        game_word_vec: Vec::new(),
+        used_letters: Vec::new()
+    };
+    hangman.create_game_word_vector(&game_word);
+
+    println!("Guess a letter!");
     for number in (1..tries).rev() {
-        guess = read_user_input();
+        let guess = Game::read_user_input();
+        hangman.check_for_letter(&guess);
         println!("You guessed {}", guess);
         print!("Keep guessing! ");
         println!("Tries left {}", number);
@@ -22,20 +27,39 @@ fn main() {
 }
 
 struct Game{
-    game_difficulty: String
-    game_word: String,
-
-
-
-
-};
-
-
-fn read_user_input() -> String{
-    let mut buffer = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Something wrong happened");
-    buffer = buffer.trim().to_string();
-    buffer
+    //game_difficulty: String,
+    game_word_vec: Vec<char>,
+    used_letters: Vec<char>,
 }
+
+impl Game{
+
+    fn read_user_input() -> char{
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Something wrong happened");
+        let mut output: Vec<char> = buffer.chars().collect();
+        output[0]
+    }
+
+
+    fn create_game_word_vector(&mut self, game_word: &String){
+        for letter in game_word.chars(){
+            self.game_word_vec.push(letter);
+        }
+    }
+
+
+    fn check_for_letter(&self, input_character: &char){
+        for index in &self.game_word_vec.len().iter(){
+            println!("{}", index);
+        }
+        
+    }
+
+    
+
+}
+
+
