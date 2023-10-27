fada3d24637b616f59ff3d5762e4e1535445c7dd
Anas Abu Dagga <abudaqa.anas@gmail.com>
2023-10-27 15:43:58 +0200

    More functionality

--- src/main.rs
+++ src/main.rs
@@ -34,28 +34,33 @@ struct Game{
 
 impl Game{
 
-    fn read_user_input() -> char{
+    fn read_user_input(&self) -> char{
+        //create a buffer string variable to store result of read_line in
         let mut buffer = String::new();
         io::stdin()
             .read_line(&mut buffer)
             .expect("Something wrong happened");
+        //collect the output in a vec of chars and return first char in it
         let mut output: Vec<char> = buffer.chars().collect();
         output[0]
+        
     }
+    
 
-
-    fn create_game_word_vector(&mut self, game_word: &String){
-        for letter in game_word.chars(){
+    fn create_game_word_vector(&mut self, chosen_word: &String){
+        //iterate over letters in chosen word and append them into our vec of chars
+        for letter in chosen_word.chars(){
             self.game_word_vec.push(letter);
         }
     }
 
 
     fn check_for_letter(&self, input_character: &char){
-        for index in &self.game_word_vec.len().iter(){
-            println!("{}", index);
+        for (index, letter) in self.game_word_vec.iter().enumerate(){
+            if input_character == letter{
+                println!("{}", index);
+            }    
         }
-        
     }
 
     
